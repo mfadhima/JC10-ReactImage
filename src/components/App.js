4a90e7fa830c3175317e281268f9e96dfacf29a0
mfadhima <mfauzanadhima@hotmail.com>
2019-08-20 15:04:23 +0700

    preventDefault()

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ import ImageList from './ImageList'
 class App extends Component {
     render() {
         return (
-            <div>
+            <div className="container">
                 <SearchBar/>
                 <ImageList/>
             </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component {
+
+    onSubmitForm = (e) => {
+        e.preventDefault()
+    }
+
+
     render() {
         return (
             <div>
-                <form>
-                    <input type="text" />
+                <h3 className="text-center mt-5">Search Image</h3>
+                <form className="form-group mt-4"
+                      onSubmit={this.onSubmitForm}>
+                    <input type="text"
+                           onChange={(e)=>{console.log(e.target.value)}}
+                           className="form-control"
+                           placeholder="Type Something" />
                 </form>
             </div>
         )
@@ -13,3 +24,9 @@ class SearchBar extends Component {
 }
 
 export default SearchBar
+
+// onSubmit, ketika tag input di dalam form di 'enter'
+    // event.preventDefault() akan menghentikan halaman utk refresh
+// onChange, ketika ada perubahan di tag input text
+    // event.target.value adalah property yg berisi teks yg kita ketik
+    // penulisan event bisa disingkat hanya dengan 'e'
